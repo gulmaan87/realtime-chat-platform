58226cb98e082654170cb20e2b2284c9a3273214
gulmaan87 <152003564+gulmaan87@users.noreply.github.com>
2026-02-02 18:23:34 +0530

    ..

--- services/frontend/src/services/socket.js
+++ services/frontend/src/services/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const SOCKET_URL = "http://localhost:3001";
+const SOCKET_URL = "https://realtime-chat-platform-socket-1.onrender.com";
 
 export const socket = io(SOCKET_URL, {
   autoConnect: false,
